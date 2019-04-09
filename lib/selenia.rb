0a2e32312ad7569c82b29ca506f91daa7f89cc21
SimplySerenity <mcgoughboy@gmail.com>
2019-04-08 21:39:00 -0700

    can now communicate with selenia server

--- lib/selenia.rb
+++ lib/selenia.rb
@@ -1,9 +1,10 @@
 require 'selenia/version'
+require 'httparty'
+require 'json'
 
 module Selenium
   module WebDriver
     class Driver
-
       # navigates to the specified url
       # waits for either the page source to stop changing or enough time to pass
       def get_fully_loaded(url, options = {})
@@ -20,6 +21,37 @@ module Selenium
           attempts += 1
         end unless source != bridge.page_source and attempts < options[:attempts]
       end
+    end
+  end
+end
+
+module Selenia
+  class Server
+    include HTTParty
+
+    def initialize(address)
+      self.class.base_uri address
+      self.class.headers 'Content-Type' => 'application/json'
+      @pool_name = ''
+    end
+
+    def join_or_init_pool(pool_name, links)
+      @pool_name = pool_name
+      self.class.post('/api/pools/' + @pool_name, body: { links: links }.to_json)
+    end
+
+    def push_links(links)
+      if @pool_name == ''
+        # something bad
+      end
+
+      reponse = self.class.post('/api/pools/' + @pool_name + '/links', body: { links: links }.to_json)
+      JSON.parse(reponse.body)
+    end
+
+    def fetch_link()
+      reponse = self.class.get('/api/pools/' + @pool_name + '/links')
+      JSON.parse(reponse.body)
     end
   end
 end
--- selenia.gemspec
+++ selenia.gemspec
@@ -36,9 +36,10 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
-  spec.add_development_dependency 'bundler', '~> 1.17'
+  spec.add_development_dependency 'bundler', '~> 2.0.1'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
 
+  spec.add_dependency 'httparty', '~> 0.16.4'
   spec.add_dependency 'selenium-webdriver', '~> 3.141.0'
 end
